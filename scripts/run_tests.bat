82bbf373136628e250fe99d8dfdc15f4f94cd521
raebmalo <70747818+raebmalo@users.noreply.github.com>
2025-08-29 15:43:37 +0200

    created batch script for windows environment

---
+++ scripts/run_tests.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem venv anlegen
+if not exist .venv (
+  %PY% -m venv .venv
+)
+
+rem venv aktivieren
+call .\.venv\Scripts\activate.bat
+
+rem pip upgraden
+python -m pip install --upgrade pip
+
+rem dependencies installieren
+pip install -e .
+
+rem Konsole clearn
+cls
+
+rem Tests starten
+python run_tests.py --model "gpt-5-2025-08-07" --cases "src\cases" --out "outputs" --limit 1
+
+endlocal
